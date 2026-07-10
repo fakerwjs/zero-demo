8d96a60ee03fa73b2f9a85d5a865d58c6896b69d
dean <1721005482@qq.com>
2026-07-10 17:58:04 +0800

    feat:first

---
+++ apps/order/rpc/internal/svc/servicecontext.go
@@ -0,0 +1,13 @@
+package svc
+
+import "github.com/fakerwjs/zero-demo/apps/order/rpc/internal/config"
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
